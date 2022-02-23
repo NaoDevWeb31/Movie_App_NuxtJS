8dc4bf6a2645a43e6ef73bfe40c3b46a6e3964ff
Naomi Mango <naomi.mango@gmail.com>
2022-02-23 14:34:27 +0100

    [MAJ] Fetch Hook
    •  Obtenir les données du film

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_SECRET}&language=fr-FR&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log(this.movies)
+    },
+  },
 }
 </script>
